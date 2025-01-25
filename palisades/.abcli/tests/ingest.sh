653f7477bc9d4b8b7936455b75f7b226dc80d42d
kamangir <arash@kamangir.net>
2025-01-25 13:32:07 -0800

    test++ - kamangir/bolt#746

--- palisades/.abcli/tests/ingest.sh
+++ palisades/.abcli/tests/ingest.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-function test_palisades_ingest() {
+function test_palisades_ingest_target_no_ingest() {
     local options=$1
 
     abcli_eval ,$options \
@@ -8,36 +8,50 @@ function test_palisades_ingest() {
         - \
         target=Palisades-Maxar-test \
         ~ingest
-    [[ $? -ne 0 ]] && return 1
+}
 
-    abcli_hr
+function test_palisades_ingest_query_object_no_ingest() {
+    local options=$1
 
     abcli_eval ,$options \
         palisades_ingest \
         - \
         $PALISADES_QUERY_OBJECT_PALISADES_MAXAR_TEST \
         ~ingest
-    [[ $? -ne 0 ]] && return 1
+}
 
-    abcli_hr
+function test_palisades_ingest_target_no_predict() {
+    local options=$1
 
     abcli_eval ,$options \
         palisades_ingest \
         - \
         target=Palisades-Maxar-test \
         scope=rgb
-    [[ $? -ne 0 ]] && return 1
+}
 
-    abcli_hr
+function test_palisades_ingest() {
+    local options=$1
+    local list_of_runners=$(abcli_option "$options" runner local+aws_batch)
 
-    abcli_eval ,$options \
-        palisades_ingest \
-        - \
-        target=Palisades-Maxar-test \
-        scope=rgb \
-        predict,count=1,~tag \
-        profile=VALIDATION \
-        - \
-        - \
-        count=3
+    local runner
+    for runner in $(echo $list_of_runners | tr + " "); do
+        abcli_log "testing runner: $runner ..."
+
+        abcli_eval ,$options \
+            palisades_ingest \
+            - \
+            target=Palisades-Maxar-test \
+            scope=rgb \
+            predict,count=1,~tag \
+            profile=VALIDATION \
+            - \
+            - \
+            count=3 \
+            to=$runner
+        [[ $? -ne 0 ]] && return 1
+
+        abcli_hr
+    done
+
 }
