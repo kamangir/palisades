c0eddb0a7cdab1034170dd88fac967ada937653c
kamangir <arash@kamangir.net>
2025-01-30 12:31:04 -0800

    doc++ - kamangir/bolt#746 - no-workflow 🪄

--- palisades/README.py
+++ palisades/README.py
@@ -2,8 +2,6 @@ import os
 
 from blue_options.help.functions import get_help
 from blue_objects import file, README
-from blue_geo import ICON as blue_geo_ICON
-from roofai import ICON as roofai_ICON
 
 from palisades.help.functions import help_functions
 from palisades import NAME, VERSION, ICON, REPO_NAME, MARQUEE
@@ -12,43 +10,36 @@ from palisades import NAME, VERSION, ICON, REPO_NAME, MARQUEE
 
 list_of_menu_item = {
     "STAC Catalog: Maxar Open Data": {
-        "ICON": blue_geo_ICON,
         "url": "https://github.com/kamangir/blue-geo/tree/main/blue_geo/catalog/maxar_open_data",
         "marquee": "https://github.com/kamangir/assets/blob/main/blue-geo/Maxar-Open-Datacube.png?raw=true",
         "title": '["Satellite imagery for select sudden onset major crisis events"](https://www.maxar.com/open-data/)',
     },
     "Vision Algo: Semantic Segmentation": {
-        "ICON": roofai_ICON,
         "url": "https://github.com/kamangir/palisades/blob/main/palisades/docs/step-by-step.md",
         "marquee": "https://github.com/kamangir/assets/raw/main/palisades/prediction-lres.png?raw=true",
         "title": "[segmentation_models.pytorch](https://github.com/qubvel-org/segmentation_models.pytorch)",
     },
     "Building Damage Analysis": {
-        "ICON": ICON,
         "url": "https://github.com/kamangir/palisades/blob/main/palisades/docs/building-analysis.md",
         "marquee": "https://github.com/kamangir/assets/blob/main/palisades/building-analysis-5.png?raw=true",
         "title": "using Microsoft, OSM, and Google footprints through [microsoft/building-damage-assessment](https://github.com/microsoft/building-damage-assessment)",
     },
     "Analytics": {
-        "ICON": ICON,
         "url": "https://github.com/kamangir/palisades/blob/main/palisades/docs/damage-analytics.md",
         "marquee": "https://github.com/kamangir/assets/blob/main/palisades/palisades-analytics-2025-01-26-17-13-55-jl0par/thumbnail-035521-377202-palisades-analytics-2025-01-26-17-13-55-jl0par.gif?raw=true",
         "title": "",
     },
     "Los Angeles Wild Fires, Jan 25": {
-        "ICON": "🔍",
         "url": "https://github.com/kamangir/palisades/blob/main/palisades/docs/WildFires-LosAngeles-Jan-2025.md",
         "marquee": "https://github.com/kamangir/assets/blob/main/palisades/palisades-analytics-2025-01-29-18-08-11-wcq26v/QGIS.png?raw=true",
         "title": "`2,685.88` sq. km = `1,148,351` buildings processed -> `10,133` with damage found.",
     },
     "Hurricane Helene, Oct 24": {
-        "ICON": "🔍",
         "url": "https://github.com/kamangir/palisades/blob/main/palisades/docs/HurricaneHelene-Oct24.md",
         "marquee": "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true",
         "title": "training the models...",
     },
     "template": {
-        "ICON": ICON,
         "url": "#",
         "marquee": "",
         "title": "",
@@ -57,8 +48,7 @@ list_of_menu_item = {
 
 
 items = [
-    "{}[`{}`]({}) [![image]({})]({}) {}".format(
-        menu_item["ICON"],
+    "[`{}`]({}) [![image]({})]({}) {}".format(
         menu_item_name,
         menu_item["url"],
         menu_item["marquee"],
